18b95e60d8c5375ce9d39b2bb2629f6b013bfa06
Masatoshi Ono <m-ono@resys.jp>
2025-07-16 17:38:51 +0900

    加速度センサーテスト中

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,20 +18,34 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        var accelerometer = Accelerometer.current;
+
+        if (accelerometer == null || !accelerometer.enabled)
+        {
+            Debug.LogWarning("Accelerometer is not available or not enabled.");
+            return;
+        }
+
+        InputSystem.EnableDevice(accelerometer);
+
+        var enabled = accelerometer.enabled;
+
+        movementX = accelerometer.acceleration.ReadValue().x;
+        movementY = accelerometer.acceleration.ReadValue().y;
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
 
-    private void Update()
-    {
-        movementX = Input.acceleration.x;
-        movementY = Input.acceleration.y;
-    }
+    //private void Update()
+    //{
+    //    movementX = Input.acceleration.x;
+    //    movementY = Input.acceleration.y;
+    //}
 
-    void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
+    //void OnMove(InputValue movementValue)
+    //{
+    //    Vector2 movementVector = movementValue.Get<Vector2>();
+    //    movementX = movementVector.x;
+    //    movementY = movementVector.y;
+    //}
 }
