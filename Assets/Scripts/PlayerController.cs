6ac002a0f5ba9b22755ef756d56a855642c0c937
Masatoshi Ono <m-ono@resys.jp>
2025-07-17 09:20:49 +0900

    iOSのDeviceSupport無効化、キーボードに戻す

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,49 +3,50 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
+    private Rigidbody _rb;
 
-    private float movementX;
-    private float movementY;
+    private float _movementX;
+    private float _movementY;
 
     public float speed = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        var accelerometer = Accelerometer.current;
+        // var accelerometer = Accelerometer.current;
 
-        if (accelerometer == null || !accelerometer.enabled)
-        {
-            Debug.LogWarning("Accelerometer is not available or not enabled.");
-            return;
-        }
+        // if (accelerometer == null || !accelerometer.enabled)
+        // {
+        //     Debug.LogWarning("Accelerometer is not available or not enabled.");
+        //     return;
+        // }
 
-        InputSystem.EnableDevice(accelerometer);
+        // InputSystem.EnableDevice(accelerometer);
 
-        var enabled = accelerometer.enabled;
+        // var enabled = accelerometer.enabled;
 
-        movementX = accelerometer.acceleration.ReadValue().x;
-        movementY = accelerometer.acceleration.ReadValue().y;
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+        // movementX = accelerometer.acceleration.ReadValue().x;
+        // movementY = accelerometer.acceleration.ReadValue().y;
+        Vector3 movement = new(_movementX, 0.0f, _movementY);
+        _rb.AddForce(movement * speed);
     }
 
-    //private void Update()
-    //{
+    // private void Update()
+    // {
     //    movementX = Input.acceleration.x;
     //    movementY = Input.acceleration.y;
-    //}
+    // }
 
-    //void OnMove(InputValue movementValue)
-    //{
-    //    Vector2 movementVector = movementValue.Get<Vector2>();
-    //    movementX = movementVector.x;
-    //    movementY = movementVector.y;
-    //}
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        Debug.Log(movementVector);
+        _movementX = movementVector.x;
+        _movementY = movementVector.y;
+    }
 }
