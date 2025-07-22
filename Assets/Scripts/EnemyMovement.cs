38da801b6eaf77e5d0b8525e1be98c120a3a5b10
Masatoshi Ono <m-ono@resys.jp>
2025-07-22 11:41:32 +0900

    逃げるを実装

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,6 +5,12 @@ public class EnemyMovement : MonoBehaviour
 {
     public Transform player;
     private NavMeshAgent _navMeshAgent;
+    public float detectionRange = 10f;  // プレイヤーを意識する距離
+
+    [SerializeField, Range(5, 30)]
+    private float _fleeDistance = 5f;     // 逃げる距離
+    public float chaseDistance = 2f;    // 奪うために近づく距離
+    public bool shouldChase = true;          // 近づくか逃げるか
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start() => _navMeshAgent = GetComponent<NavMeshAgent>();
@@ -14,7 +20,30 @@ public class EnemyMovement : MonoBehaviour
     {
         if (player != null)
         {
-            _navMeshAgent.SetDestination(player.position);
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+            if (shouldChase)
+            {
+                Debug.Log("追いかけます");
+                _ = _navMeshAgent.SetDestination(player.position);
+            }
+            else
+            {
+                if (distanceToPlayer < detectionRange)
+                {
+                    Debug.Log("逃げます");
+                    Vector3 fleeDirection = (transform.position - player.position).normalized;
+                    Vector3 targetPosition = transform.position + (fleeDirection * _fleeDistance);
+                    if (NavMesh.SamplePosition(targetPosition, out NavMeshHit hit, 2f, NavMesh.AllAreas))
+                    {
+                        _ = _navMeshAgent.SetDestination(hit.position);
+                    }
+                }
+                else
+                {
+                    _navMeshAgent.ResetPath();
+                }
+            }
         }
     }
 }
--- Assets/Scripts/RandomObject.cs
+++ Assets/Scripts/RandomObject.cs
@@ -10,7 +10,7 @@ public class RandomObject : MonoBehaviour
     public GameObject prefabDynamicBox;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
         for (int i = 0; i < pickUpBoxCount; i++)
         {
